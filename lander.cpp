b16968f10e006321d9601ffb19e0a90792f9353a
Dexter <dexholst12@gmail.com>
2023-04-28 12:04:43 -0400

    tasks

---
+++ lander.cpp
@@ -0,0 +1,19 @@
+//TO DO
+
+//Draw to screen
+//Flat terrain
+//Lander object
+//Controllable lander
+//Accurate physics
+//Collision, landing condition
+//Varied terrain generation
+
+#include<cstdlib>
+#include<iostream>
+
+
+int main(){
+
+    std::cout << "test" << std::endl;
+
+}
